3fdf179576561675e669bfbc48bec5e0ef7f732c
BuyTheLook <buythelookmanage@gmail.com>
2025-04-01 23:15:27 +0300

    Update lookService.ts

--- src/services/lookService.ts
+++ src/services/lookService.ts
@@ -1,15 +1,13 @@
-
 /**
  * Main look service that exports all necessary functions for outfit generation
  */
 
 import { fetchFirstOutfitSuggestion } from './outfitService';
-import { fetchItemsForOccasion, getAllItemsFromSupabase } from './dashboardService';
+import { fetchItemsForOccasion } from './dashboardService';
 import { mapDashboardItemToOutfitItem } from './mappers/styleMappers';
 
 export {
   fetchFirstOutfitSuggestion,
   fetchItemsForOccasion,
-  getAllItemsFromSupabase,
   mapDashboardItemToOutfitItem
 };
